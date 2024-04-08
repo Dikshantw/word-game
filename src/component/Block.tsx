a727d1ae09ff6e729c105466db06309ed02fd0cf
Dikshantw <dikshantw11@gmail.com>
2024-04-08 14:46:10 +0530

    shows the user guesses in the blocks

---
+++ src/component/Block.tsx
@@ -0,0 +1,16 @@
+const Block = ({ word }) => {
+  return (
+    <p className="flex gap-1 mb-1">
+      {word.split("").map((letter) => (
+        <span
+          key={letter}
+          className="relative w-20 grid place-content-center border-2 border-gray-700 text-2xl"
+        >
+          {letter}
+        </span>
+      ))}
+    </p>
+  );
+};
+
+export default Block;
--- src/component/GuessResult.tsx
+++ src/component/GuessResult.tsx
@@ -1,21 +1,11 @@
+import Block from "./Block";
+
 const GuessResult = ({ guesses }) => {
   return (
     <>
       <div className="flex-1 flex flex-col justify-center">
-        {/* {[0, 1, 2, 3, 4, 5].map((word) => (
-          <p key={word} className="flex gap-1 mb-1">
-            {[0, 1, 2, 3, 4].map((letter) => (
-              <span
-                key={letter}
-                className="relative w-20 grid place-content-center border-2 border-gray-700 text-2xl"
-              >
-                
-              </span>
-            ))}
-          </p>
-        ))} */}
         {guesses.map((guess) => (
-          <p className="flex gap-1 mb-1">{guess}</p>
+          <Block key={guess} word={guess} />
         ))}
       </div>
     </>
